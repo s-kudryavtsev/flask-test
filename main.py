a75053dda4bfcb97a656244716821342c16ee1e4
Sergey Kudryavtsev <snkudryavtsev@yandex.ru>
2023-07-27 14:43:37 +0300

    логи

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request, redirect 
 from flask_sqlalchemy import SQLAlchemy 
+import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
 app = Flask(__name__) 
@@ -9,6 +17,18 @@ db = SQLAlchemy(app)
 
 app_context = app.app_context()
 app_context.push()
+
+
+logs_path = 'c:\logs\\'
+
+os.makedirs(logs_path, exist_ok=True)
+logging.basicConfig(level=logging.DEBUG,
+                    handlers=[TimedRotatingFileHandler(logs_path + '/app.log', when='D', interval=1, backupCount=10)],
+                    format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+                    datefmt='%Y-%m-%dT%H:%M:%S'
+                    )
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.DEBUG)
 
 
 class Item(db.Model): 
